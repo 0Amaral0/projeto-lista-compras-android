c697d4145757f2b3cd65e34f964005655aea735b
Davi Amaral <dasoitaperuna@gmail.com>
2022-04-26 21:35:59 -0300

    Tela de lista de itens

---
+++ app/src/main/java/com/example/lista_compras_crud/ui/item_shop_list/ItemShopListFragment.kt
@@ -0,0 +1,8 @@
+package com.example.lista_compras_crud.ui.item_shop_list
+
+import androidx.fragment.app.Fragment
+import com.example.lista_compras_crud.R
+
+class ItemShopListFragment : Fragment(R.layout.item_shop_list_fragment) {
+
+}
---
+++ app/src/main/java/com/example/lista_compras_crud/ui/item_shop_list/ItemShopListViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.lista_compras_crud.ui.item_shop_list
+
+import androidx.lifecycle.ViewModel
+
+class ItemShopListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
